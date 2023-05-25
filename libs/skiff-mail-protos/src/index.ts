472516fa4702518d2a74b4046e8f953b57ef090c
Andrew Milich <milichab@gmail.com>
2023-05-25 13:27:57 -0700

    release skiff-mail-protos

---
+++ libs/skiff-mail-protos/src/index.ts
@@ -0,0 +1,10 @@
+export * from '../generated/protos/com/skiff/skemail/encrypted/encrypted_data';
+export {
+  MailFilterHeader,
+  MailFilterBody,
+  FilterActionHeader,
+  FilterActionBody,
+  SingleFilterAction,
+  FilterSerializedDataHeader,
+  FilterSerializedDataBody
+} from '../generated/protos/com/skiff/skemail/filters/filters';
