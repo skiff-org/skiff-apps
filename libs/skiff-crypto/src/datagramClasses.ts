ad76e0c203195d77362b2c44fbbd53988cf20134
Richard Liu <rliu@nightwatchapp.com>
2023-06-13 15:59:22 -0700

    add v2 functions to skiff-crypto

---
+++ libs/skiff-crypto/src/datagramClasses.ts
@@ -0,0 +1,21 @@
+import { Reader, Writer } from 'protobufjs';
+import { Range } from 'semver';
+
+import { DatagramV2 } from './aead-v2/common';
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export const createProtoWrapperDatagram = <Header, Body>(
+  type: string,
+  headerClass: { encode(input: Header, writer: Writer): Writer; decode(input: Reader, lenght?: number): Header },
+  bodyClass: { encode(input: Body, writer: Writer): Writer; decode(input: Reader, lenght?: number): Body },
+  version = '0.1.0',
+  versionConstraint = new Range('0.1.*')
+): DatagramV2<Header, Body> => ({
+  versionConstraint,
+  version,
+  type,
+  serializeHeader: (header: Header) => headerClass.encode(header, Writer.create()).finish(),
+  deserializeHeader: (data: Uint8Array) => headerClass.decode(Reader.create(data)),
+  serializeBody: (body: Body) => bodyClass.encode(body, Writer.create()).finish(),
+  deserializeBody: (data: Uint8Array) => bodyClass.decode(Reader.create(data))
+});
