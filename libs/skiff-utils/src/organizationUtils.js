d6df1bba9c7d54d0c2de2ada30ce5dff42cf2f9b
Richard Liu <rliu@nightwatchapp.com>
2023-07-10 18:38:08 +0100

    ignore .js and .js.map generated

--- libs/skiff-utils/src/organizationUtils.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.WORKSPACE_NAME_MAX_LENGTH = void 0;
-exports.WORKSPACE_NAME_MAX_LENGTH = 26;
-//# sourceMappingURL=organizationUtils.js.map
